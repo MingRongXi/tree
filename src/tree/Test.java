bcdafd662a496b5afd60369135d5f5af7a5bf7da
MingRongXi <zhaolei7678@outlook.com>
2018-10-29 23:18:46 +0800

    test

--- src/tree/Test.java
+++ src/tree/Test.java
@@ -4,11 +4,13 @@ import tree.Tree.Node;
 
 public class Test {
 	
-	private static String testString = "1234**5***67**8**";
+	private static String testString = "1234**59****67**8**";
 
 	public static void main(String[] args) {
-		//testOutput();
-		testGetParent('5');
+		testOutput();
+//		testGetParent('9');
+		System.out.println("----------------");
+		test1();
 	}
 	
 	public static <E> void testOutput() {
@@ -25,5 +27,15 @@ public class Test {
 		Tree<E> testTree = new Tree<>(testString);
 		Node<E> result = testTree.getParent(c);
 		System.out.println(result.getData());
+	}
+	
+	public static void test1() {
+		Tree2<Character> testTree = Tree2.createTreeFromString(testString);
+		testTree.pre();
+		System.out.println();
+		testTree.mid();
+		System.out.println();
+		testTree.end();
+		System.out.println();	
 	}
 }
